f8491ed3c1db22e221fdf6837aeb3696211d867b
james5418 <james900425@gmail.com>
2023-03-12 23:05:50 +0800

    [refactor] change package name

--- app/src/main/java/edu/mui/noti/summary/util/LogUtil.kt
+++
@@ -1,8 +0,0 @@
-package edu.mui.noti.summary.util
-
-// import this package, and can directly use TAG in Log function
-val Any.TAG: String
-    get() {
-        val tag = javaClass.simpleName
-        return if (tag.length <= 23) tag else tag.substring(0, 23)
-    }
