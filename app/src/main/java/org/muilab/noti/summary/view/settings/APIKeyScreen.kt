c9a171603bbdf5f428d6a8a6d35a7146791f4590
james5418 <james900425@gmail.com>
2023-03-27 16:35:28 +0800

    [feat] AddKeyButton

--- app/src/main/java/org/muilab/noti/summary/view/settings/APIKeyScreen.kt
+++ app/src/main/java/org/muilab/noti/summary/view/settings/APIKeyScreen.kt
@@ -2,25 +2,31 @@ package org.muilab.noti.summary.view.settings
 
 import android.app.Application
 import android.content.Context
+import android.content.SharedPreferences
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.foundation.Image
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
+import org.muilab.noti.summary.R
 import org.muilab.noti.summary.util.getActiveNotifications
 
 @Composable
 fun APIKeyScreen(context: Context) {
-    Column(modifier = Modifier.fillMaxHeight()) { TextBoxForSetAPI(context) }
-}
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun TextBoxForSetAPI(context: Context) {
     val sharedPref = context.getSharedPreferences("SummaryPref", Context.MODE_PRIVATE)
-    var apiKey: String by remember { mutableStateOf("") }
-    var displayApiKey by remember {
+
+    val displayApiKey = remember {
         val apiKeyInSharedPref =  sharedPref.getString("userAPIKey", "")
         if (apiKeyInSharedPref == "")
             mutableStateOf("sk-********")
@@ -28,45 +34,205 @@ fun TextBoxForSetAPI(context: Context) {
             mutableStateOf("sk-****" + apiKeyInSharedPref!!.takeLast(4))
     }
 
-    fun updateApiKey(newApiKey: String) {
-        if (!newApiKey.startsWith("sk-")) {
-            // TODO: Show users the API key is in the wrong format
-            return
-        }
-
-        with(sharedPref.edit()) {
-            putString("userAPIKey", newApiKey)
-            apply()
-        }
-
-        val lastPartOfApiKey = newApiKey.takeLast(4)
-        displayApiKey = displayApiKey.dropLast(4) + lastPartOfApiKey
-    }
-
-    Row(modifier = Modifier.fillMaxWidth()) {
-        TextField(
-            value = apiKey,
-            onValueChange = { apiKey = it },
-            label = { Text("API 金鑰") },
-            placeholder = { Text("輸入您的 API 金鑰") },
-            modifier = Modifier.fillMaxWidth(0.7F),
-        )
-
-        Button(
-            onClick = {
-                updateApiKey(apiKey)
-                apiKey = ""
-            }) {
-              Text("更新")
-            }
-        }
-    Row(modifier = Modifier.fillMaxWidth()) {
-          if (displayApiKey == "sk-********") {
-              Text("OpenAI 金鑰格式：")
-          } else {
-              Text("您的 OpenAI 金鑰：")
-          }
-          Text(displayApiKey)
+    MaterialTheme {
+//        Column(modifier = Modifier.fillMaxHeight()) { TextBoxForSetAPI(context, displayApiKey) }
+        Text(displayApiKey.value)
+        AddKeyButton(sharedPref, displayApiKey)
     }
 
 }
+
+
+@Composable
+fun APIKeyList() {
+
+}
+
+
+@Composable
+fun AddKeyButton(sharedPref: SharedPreferences, displayApiKey: MutableState<String>,) {
+
+    val showDialog = remember { mutableStateOf(false) }
+    val inputKey = remember { mutableStateOf("") }
+
+    Box(
+        modifier = Modifier.fillMaxSize().padding(bottom = 120.dp, end = 120.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        FloatingActionButton(
+            onClick = { showDialog.value = true },
+        ) {
+            Icon(Icons.Filled.Add, "add new key")
+        }
+    }
+
+    val confirmAction = {
+        if (inputKey.value != "") {
+
+            with(sharedPref.edit()) {
+                putString("userAPIKey", inputKey.value)
+                apply()
+            }
+
+            val lastPartOfApiKey = inputKey.value.takeLast(4)
+            displayApiKey.value = displayApiKey.value.dropLast(4) + lastPartOfApiKey
+
+            inputKey.value = ""
+            showDialog.value = false
+        }
+    }
+
+    val dismissAction = { inputKey.value = "" }
+
+    if (showDialog.value) {
+        APIKeyEditor(showDialog, inputKey, confirmAction, dismissAction)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun APIKeyEditor(
+    showDialog: MutableState<Boolean>,
+    defaultPromptInTextBox: MutableState<String>,
+    confirmAction: () -> Unit,
+    dismissAction: () -> Unit = {},
+) {
+    NoPaddingAlertDialog(
+        title = {
+            Image(
+                modifier = Modifier.fillMaxWidth().padding(top = 30.dp, bottom = 20.dp).height(70.dp),
+                painter = painterResource(id = R.drawable.key),
+                contentDescription = "key_icon",
+            )
+        },
+        text = {
+            OutlinedTextField(
+                modifier = Modifier.padding(start = 20.dp, end = 20.dp).fillMaxWidth(),
+                singleLine = true,
+                value = defaultPromptInTextBox.value,
+                onValueChange = { defaultPromptInTextBox.value = it },
+                label = { Text("API 金鑰") },
+            )
+        },
+        confirmButton = {
+            TextButton(
+                modifier = Modifier.padding(all = 3.dp),
+                onClick = {
+                    confirmAction()
+                }
+            )
+            { Text(text = "確認", modifier = Modifier.padding(start = 30.dp, end = 30.dp)) }
+        },
+        dismissButton = {
+            TextButton(
+                modifier = Modifier.padding(all = 3.dp),
+                onClick = {
+                    dismissAction()
+                    showDialog.value = false
+                }
+            )
+            { Text(text = "取消", modifier = Modifier.padding(start = 30.dp, end = 30.dp)) }
+        }
+    )
+}
+
+@Composable
+fun NoPaddingAlertDialog(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit = {},
+    title: @Composable (() -> Unit)? = null,
+    text: @Composable (() -> Unit)? = null,
+    confirmButton: @Composable () -> Unit,
+    dismissButton: @Composable (() -> Unit)? = null,
+    shape: Shape = MaterialTheme.shapes.medium,
+    properties: DialogProperties = DialogProperties()
+) {
+    Dialog(
+        onDismissRequest = onDismissRequest,
+        properties = properties
+    ) {
+        Surface(
+            modifier = modifier,
+            shape = shape,
+        ) {
+            Column(
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                title?.let {
+                    CompositionLocalProvider() {
+                        val textStyle = MaterialTheme.typography.titleLarge
+                        ProvideTextStyle(textStyle, it)
+                    }
+                }
+                text?.let {
+                    CompositionLocalProvider() {
+                        val textStyle = MaterialTheme.typography.labelSmall
+                        ProvideTextStyle(textStyle, it)
+                    }
+                }
+                Box(
+                    Modifier.fillMaxWidth().padding(all = 6.dp)
+                ) {
+                    Row(
+                        horizontalArrangement = Arrangement.SpaceAround,
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        dismissButton?.invoke()
+                        confirmButton()
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+//@OptIn(ExperimentalMaterial3Api::class)
+//@Composable
+//fun TextBoxForSetAPI(context: Context, displayApiKey: MutableState<String>) {
+//    val sharedPref = context.getSharedPreferences("SummaryPref", Context.MODE_PRIVATE)
+//    var apiKey: String by remember { mutableStateOf("") }
+//
+//    fun updateApiKey(newApiKey: String) {
+//        if (!newApiKey.startsWith("sk-")) {
+//            // TODO: Show users the API key is in the wrong format
+//            return
+//        }
+//
+//        with(sharedPref.edit()) {
+//            putString("userAPIKey", newApiKey)
+//            apply()
+//        }
+//
+//        val lastPartOfApiKey = newApiKey.takeLast(4)
+//        displayApiKey.value = displayApiKey.value.dropLast(4) + lastPartOfApiKey
+//    }
+//
+//    Row(modifier = Modifier.fillMaxWidth()) {
+//        TextField(
+//            value = apiKey,
+//            onValueChange = { apiKey = it },
+//            label = { Text("API 金鑰") },
+//            placeholder = { Text("輸入您的 API 金鑰") },
+//            modifier = Modifier.fillMaxWidth(0.7F),
+//        )
+//
+//        Button(
+//            onClick = {
+//                updateApiKey(apiKey)
+//                apiKey = ""
+//            }) {
+//              Text("更新")
+//            }
+//        }
+//    Row(modifier = Modifier.fillMaxWidth()) {
+//          if (displayApiKey.value == "sk-********") {
+//              Text("OpenAI 金鑰格式：")
+//          } else {
+//              Text("您的 OpenAI 金鑰：")
+//          }
+//          Text(displayApiKey.value)
+//    }
+//
+//}
