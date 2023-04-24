1bb753df75965832a356148b335ca8418f131735
Uei-Dar Chen <ueidarchen@gmail.com>
2023-04-24 19:09:53 +0800

    [refactor] Delete CurrentDrawer

--- app/src/main/java/org/muilab/noti/summary/database/room/PromptDatabase.kt
+++ app/src/main/java/org/muilab/noti/summary/database/room/PromptDatabase.kt
@@ -4,7 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import org.muilab.noti.summary.model.CurrentDrawer
 import org.muilab.noti.summary.model.Prompt
 
 @Database(entities = [Prompt::class], version = 1)
