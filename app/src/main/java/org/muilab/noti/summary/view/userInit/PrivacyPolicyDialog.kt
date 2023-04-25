9398eebc34970aa7bfd58a9226ae1ef7f1236eca
Uei-Dar Chen <ueidarchen@gmail.com>
2023-04-25 10:30:19 +0800

    [feat] Add privacy policy to init dialog

--- app/src/main/java/org/muilab/noti/summary/view/userInit/PrivacyPolicyDialog.kt
+++ app/src/main/java/org/muilab/noti/summary/view/userInit/PrivacyPolicyDialog.kt
@@ -1,6 +1,8 @@
 package org.muilab.noti.summary.view.userInit
 
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
@@ -8,18 +10,30 @@ import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.compose.ui.window.DialogProperties
 import org.muilab.noti.summary.R
 
+class CustomWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+        view?.loadUrl(request?.url.toString())
+        return true
+    }
+}
+
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 fun PrivacyPolicyDialog(onAgree: () -> Unit) {
     var agree by remember { mutableStateOf(false) }
-    val privacyURL = "https://www.example.com" // TODO: Change in the future
+    val privacyURL = stringResource(R.string.privacy_URL) // TODO: Change in the future
+    val screenWidth = LocalConfiguration.current.screenWidthDp.dp
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+    val privacyWidth = screenWidth * 4 / 5
     val privacyHeight = screenHeight * 3 / 5
 
     AlertDialog(
@@ -32,10 +46,12 @@ fun PrivacyPolicyDialog(onAgree: () -> Unit) {
                     AndroidView(
                         factory = {
                             WebView(it).apply {
+                                webViewClient = CustomWebViewClient()
+                                settings.javaScriptEnabled = true
                                 loadUrl(privacyURL)
                             }
                         },
-                        modifier = Modifier.height(privacyHeight)
+                        modifier = Modifier.size(privacyWidth, privacyHeight)
                     )
                 }
                 Row (verticalAlignment = Alignment.CenterVertically) {
@@ -51,6 +67,9 @@ fun PrivacyPolicyDialog(onAgree: () -> Unit) {
             Button(onClick = onAgree, enabled = agree) {
                 Text(stringResource(R.string.ok))
             }
-        }
+        },
+        properties = DialogProperties(
+            usePlatformDefaultWidth = false
+        ),
     )
 }
